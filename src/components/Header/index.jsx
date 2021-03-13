f09b45442bf2629e17e7e5f020d50291217b0c85
kross <662242@mail.ru>
2021-03-14 00:04:38 +0500

    lesson2

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+
+import s from  './index.module.css';
+
+const Header =() => {
+    
+return(
+    <header className={s.root}>
+    <div className={s.forest}></div>
+    <div className={s.container}>
+        <h1>This is title</h1>
+        <p>This is Description!</p>
+    </div>
+</header>
+)
+}
+export default Header;
