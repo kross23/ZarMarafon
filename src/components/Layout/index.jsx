41ee665b05be0c33a17f71d3441adeaaf7a10ab7
kross <662242@mail.ru>
2021-03-18 01:02:53 +0500

    pull

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -14,9 +14,9 @@ return(
     <section className={l.root} style={styles} >
     <div className={l.wrapper}>
         <article >
-            <div className={l.title}>
+            <div className={l.title} >
                 <h3>{title}</h3>
-                <span className={l.separator}></span>
+                <span className={l.separator} ></span>
             </div>
             <div className={names_cn(`${l.desc} `,`${l.full}`)}>
               
