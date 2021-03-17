9cabe2b5d35974a75e5bb621a5070c4855c14b0a
kross <662242@mail.ru>
2021-03-18 00:35:52 +0500

    lesson-5

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,41 +1,51 @@
+import { Link } from 'react-router-dom';
 import menuStyle from './style.module.css';
 import names_cn from 'classnames';
 
-const Menu = ({handleChangePage,navState}) => {
-    const heandleMenu = (e) => {
-      let target = e.target.textContent.toLowerCase();
-      handleChangePage && handleChangePage(target);
-    } 
-
+const Menu = ({navState, navHeandle}) => {
+  const heandleMenu = () => {
+    
+    navHeandle && navHeandle();
+  }
+const MENU =[
+  {
+    title:"HOME",
+    to:"home",
+  },
+  {
+    title:"GAME",
+    to:"game",
+  },
+  {
+    title:"ABOUT",
+    to:"about",
+  },
+  {
+    title:"CONTACT",
+    to:"contact",
+  },
+]
     return(
-        <div className={names_cn(menuStyle.menuContainer,{[menuStyle.active]:navState})}>
+        <div className={names_cn(menuStyle.menuContainer,{[menuStyle.active]:
+          navState===true,[menuStyle.deactive]:navState===false})
+        }>
         <div className={menuStyle.overlay} />
         <div className={menuStyle.menuItems}>
-          <ul onClick={heandleMenu} >
-            <li>
-              <a href="#welcome">
-                HOME
-              </a>
-            </li>
-            <li>
-              <a href="#game">
-                GAME
-              </a>
-            </li>
-            <li>
-              <a href="#about">
-                ABOUT
-              </a>
-            </li>
-            <li>
-              <a href="#contact">
-                CONTACT
-              </a>
-            </li>
+          <ul >
+          {
+            MENU.map(({title, to}, index) => (
+              <li key = {index} onClick={heandleMenu}>
+              <Link to={to}>
+                {title}
+                  </Link>
+                  </li>
+            ))
+          }
+
           </ul>
         </div>
       </div>
-        
+
     )
 }
 export default Menu;
